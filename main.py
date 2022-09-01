4de1c3766d7948ac4c7626f5d0a9d19aa21248cb
Timmo Hage <attmhage@gmail.com>
2022-09-01 14:41:48 +0200

    Add still_has_answers method

--- main.py
+++ main.py
@@ -10,4 +10,6 @@ for question in question_data:
     question_bank.append(new_question)
 
 quiz = QuizLogic(question_bank)
-quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()
--- quiz_logic.py
+++ quiz_logic.py
@@ -4,7 +4,10 @@ class QuizLogic:
         self.question_number = 0
         self.list = question_list
 
+    def still_has_questions(self):
+        return len(self.list) > self.question_number
+
     def next_question(self):
-        number = self.question_number
-        question = self.list[number].text
-        input(f'Q.{number + 1}: {question} (True/False)?')
+        current_question = self.list[self.question_number].text
+        self.question_number += 1
+        input(f'Q.{self.question_number}: {current_question} (True/False)?')
