7e59acac563c05399046d0d232daac2f4e25a893
Timmo Hage <attmhage@gmail.com>
2022-09-01 14:57:17 +0200

    Add logic for checking answer

--- quiz_logic.py
+++ quiz_logic.py
@@ -1,13 +1,25 @@
 class QuizLogic:
 
     def __init__(self, question_list):
+        self.score = 0
         self.question_number = 0
         self.list = question_list
+
 
     def still_has_questions(self):
         return len(self.list) > self.question_number
 
     def next_question(self):
-        current_question = self.list[self.question_number].text
+        current_question = self.list[self.question_number]
         self.question_number += 1
-        input(f'Q.{self.question_number}: {current_question} (True/False)?')
+        answer = input(f'Q.{self.question_number}: {current_question.text} (True/False)? ')
+        self.check_answer(answer, current_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print('Correct!')
+            self.score += 1
+        else:
+            print('That is wrong.')
+        print(f'The correct answer was {correct_answer}.')
+        print(f'Your current score is {self.score}/{self.question_number}')
